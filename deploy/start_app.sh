aa409eb864c613d5d31e25c455eb2f30913cbdb9
shlee <sanghoon.lee@mz.co.kr>
2022-11-03 16:05:06 +0900

    JS-2 TEST

--- deploy/start_app.sh
+++ deploy/start_app.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-cd /home/ec2-user/webapps/quiz/deploy
-pm2 serve --name quiz-app /home/ec2-user/webapps/quiz/deploy/build 3000
+cd /home/ec2-user/webapps/quiz && pm2 serve --name quiz-app build 3000
